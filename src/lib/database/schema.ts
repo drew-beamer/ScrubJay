19c7fb612c1994bb69b6dc9a849a2a9898b3c34f
Drew Beamer <andrewmbeamer@gmail.com>
2024-09-10 23:01:23 -0400

    clean up schema

--- src/lib/database/schema.ts
+++ src/lib/database/schema.ts
@@ -1,19 +1,19 @@
-import { integer, text, sqliteTable, real } from 'drizzle-orm/sqlite-core';
+import { integer, text, sqliteTable, real, primaryKey } from 'drizzle-orm/sqlite-core';
 
 export const locations = sqliteTable('locations', {
     id: text('id').primaryKey(),
     county: text('county').notNull(),
     state: text('state').notNull(),
     lat: real('lat').notNull(),
-    long: real('long').notNull(),
+    lng: real('lng').notNull(),
     name: text('name').notNull(), // hotspot or private location name on ebird
-    isPrivate: integer('is_private').notNull()
+    isPrivate: integer('is_private', { mode: 'boolean' }).notNull()
 });
 
 export const observations = sqliteTable('observations', {
-    id: integer('id').primaryKey({ autoIncrement: true }),
     comName: text('common_name').notNull(),
     sciName: text('scientific_name').notNull(),
+    speciesCode: text('species_code').notNull(),
     locId: text('location_id')
         .references(() => locations.id, {
             onDelete: 'cascade',
@@ -21,13 +21,15 @@ export const observations = sqliteTable('observations', {
         })
         .notNull(),
     obsDt: integer('observation_date', { mode: 'timestamp' }).notNull(),
+    createdDt: integer('creation_date', {mode: 'timestamp'}).notNull(),
     howMany: integer('how_many').notNull(),
     obsValid: integer('observation_valid', { mode: 'boolean' }).notNull(),
     obsReviewed: integer('observation_reviewed', { mode: 'boolean' }).notNull(),
     subId: text('sub_id').notNull(),
-    obsId: text('obs_id').notNull(),
-    checklistId: text('checklist_id').notNull(),
-    presenceNoted: integer('presenceNoted', { mode: 'boolean' }).notNull(),
+    presenceNoted: integer('presence_noted', { mode: 'boolean' }).notNull(),
     hasComments: integer('has_comments', { mode: 'boolean' }).notNull(),
     evidence: text('evidence', { enum: ['P', 'A', 'V'] }),
-});
+
+}, (table) => ({
+    pk: primaryKey({ columns: [table.speciesCode, table.subId, table.evidence]})
+}));
